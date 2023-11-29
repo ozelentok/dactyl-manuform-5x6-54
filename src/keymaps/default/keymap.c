8f8e267ea6b60cd5105912afc0a8f8d43055d333
Oz Elentok <oz.elen@gmail.com>
2023-11-29 15:58:48 +0200

    chore: Update keymap layout

--- src/keymaps/default/keymap.c
+++ src/keymaps/default/keymap.c
@@ -52,23 +52,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [SYM] = LAYOUT_5x6_54(
         KC_TRANSPARENT,  KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                      KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_CAPS,
-        KC_TRANSPARENT,  KC_F11,          KC_F12,          KC_TILD,         KC_COLON,        CW_TOGG,                                                    KC_DQT,          KC_QUOT,         KC_LBRC,         KC_RBRC,         KC_BSLS,         KC_INSERT,
-        KC_TRANSPARENT,  LGUI_T(KC_TAB),  LALT_T(KC_TAB),  LSFT_T(KC_GRAVE),LCTL_T(KC_MINUS),KC_UNDS,                                                    KC_LEFT,         LCTL_T(KC_DOWN), LSFT_T(KC_UP),   LALT_T(KC_RIGHT),KC_PIPE,         KC_ENT,
-        KC_TRANSPARENT,  ALT_SHIFT,       KC_ESC,          KC_EXCLAIM,      KC_EQL,          KC_PLUS,                                                    KC_HOME,         KC_PGDN,         KC_PGUP,         KC_END,          ALT_SHIFT,       KC_PAUSE,
+        KC_TRANSPARENT,  KC_F11,          KC_F12,          KC_EXCLAIM,      KC_DQT,          CW_TOGG,                                                    ALT_SHIFT,       KC_QUOT,         KC_LBRC,         KC_RBRC,         KC_BSLS,         KC_INSERT,
+        KC_TRANSPARENT,  LGUI_T(KC_TAB),  LALT_T(KC_TAB),  LSFT_T(KC_GRAVE),LCTL_T(KC_MINUS),KC_UNDS,                                                    KC_LEFT,         LCTL_T(KC_DOWN), LSFT_T(KC_UP),   LALT_T(KC_RIGHT),KC_PIPE,         KC_TRANSPARENT,
+        KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TILD,         KC_EQL,          KC_PLUS,                                                    KC_HOME,         KC_PGDN,         KC_PGUP,         KC_END,          KC_TRANSPARENT,  KC_PAUSE,
                                                                             KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,           KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT
     ),
     [GAME] = LAYOUT_5x6_54(
-        KC_TRANSPARENT,  KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                                                       KC_6,            KC_7,            KC_8,            KC_9,            KC_0,            KC_PSCR,
+        KC_ESC,          KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                                                       KC_6,            KC_7,            KC_8,            KC_9,            KC_0,            KC_PSCR,
         KC_TRANSPARENT,  KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                       KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,            TG(GAME),
-        KC_TRANSPARENT,  KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                       KC_H,            KC_J,            KC_K,            KC_L,            KC_SCLN,         KC_QUOT,
-        KC_TRANSPARENT,  KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,                                                       KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,         KC_RCTL,
+        KC_LSFT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                       KC_H,            KC_J,            KC_K,            KC_L,            KC_SCLN,         KC_QUOT,
+        KC_LCTL,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,                                                       KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,         KC_RCTL,
                                                                             KC_LGUI,         KC_SPC,          LALT_T(KC_ESC),           KC_ENT,          KC_BSPC,         KC_DEL
+        /* Experimental shifted WASD layout
+        // KC_TRANSPARENT,  KC_TAB,          KC_Q,            KC_W,            KC_E,            KC_R,
+        // KC_LSFT,         KC_LSFT,         KC_A,            KC_S,            KC_D,            KC_F,
+        // KC_LCTL,         KC_LCTL,         KC_Z,            KC_X,            KC_C,            KC_V,
+        */
     ),
     [MEDIA] = LAYOUT_5x6_54(
         KC_TRANSPARENT,  KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                      KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F11,
-        KC_TRANSPARENT,  KC_VOLD,         KC_VOLU,         KC_MUTE,         TAB_AND_REFRESH, TG(GAME),                                                   KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_F12,
-        KC_TRANSPARENT,  KC_MPLY,         KC_MPRV,         KC_MNXT,         PASTE_ENTER,     KC_TAB,                                                     KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
-        KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,                                             KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
-                                                                            QK_BOOT,         KC_TRANSPARENT,  KC_TRANSPARENT,           KC_TRANSPARENT,  KC_TRANSPARENT,  QK_BOOT
+        KC_TRANSPARENT,  KC_VOLD,         KC_VOLU,         KC_MUTE,         TAB_AND_REFRESH, KC_F12,                                                     ALT_SHIFT,       KC_7,            KC_8,            KC_9,            KC_TRANSPARENT,  KC_TRANSPARENT,
+        KC_TRANSPARENT,  KC_MPLY,         KC_MPRV,         KC_MNXT,         PASTE_ENTER,     KC_TRANSPARENT,                                             KC_TRANSPARENT,  KC_4,            KC_5,            KC_6,            KC_TRANSPARENT,  KC_TRANSPARENT,
+        KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,                                             KC_TRANSPARENT,  KC_1,            KC_2,            KC_3,            KC_TRANSPARENT,  KC_TRANSPARENT,
+                                                                            TG(GAME),        KC_TRANSPARENT,  QK_BOOT,                  QK_BOOT,         KC_0,            KC_TRANSPARENT
     ),
 };
