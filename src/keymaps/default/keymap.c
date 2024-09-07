e05e016d549b5f8fa0969d14180f5abf06c3d52b
Oz Elentok <oz.elen@gmail.com>
2024-09-07 22:13:13 +0300

    feat: Add RGB Lighting for layers

--- src/keymaps/default/keymap.c
+++ src/keymaps/default/keymap.c
@@ -2,6 +2,17 @@
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
+#define BASE 0
+#define NAV 1
+#define SYM 2
+#define MOUSE 3
+#define GAME 4
+#define GAME_S 5
+#define MEDIA 6
+
+#define U_UND LCTL(KC_Z)
+#define U_RDO LCTL(KC_Y)
+
 enum custom_keycodes {
   ALT_SHIFT = SAFE_RANGE,
   TAB_AND_REFRESH,
@@ -40,13 +51,32 @@ const uint16_t PROGMEM combo_cw_toggle[] = {KC_M, KC_COMM, COMBO_END};
 // clang-format off
 
 combo_t key_combos[] = {
-    COMBO(combo_tab, KC_TAB),
-    COMBO(combo_alt_shift, ALT_SHIFT),
-    COMBO(combo_alt_tab, LALT(KC_TAB)),
-    COMBO(combo_gui_d, LGUI(KC_D)),
-    COMBO(combo_cw_toggle, CW_TOGG),
+  COMBO(combo_tab, KC_TAB),
+  COMBO(combo_alt_shift, ALT_SHIFT),
+  COMBO(combo_alt_tab, LALT(KC_TAB)),
+  COMBO(combo_gui_d, LGUI(KC_D)),
+  COMBO(combo_cw_toggle, CW_TOGG),
 };
 
+// clang-format on
+
+const rgblight_segment_t PROGMEM game_layer_rgb[] =
+    RGBLIGHT_LAYER_SEGMENTS({0, 1, 85, 255, 20});
+const rgblight_segment_t PROGMEM game_s_layer_rgb[] =
+    RGBLIGHT_LAYER_SEGMENTS({0, 1, 170, 255, 20});
+
+const rgblight_segment_t *const PROGMEM my_rgb_layers[] =
+    RGBLIGHT_LAYERS_LIST(game_layer_rgb, game_s_layer_rgb);
+
+void keyboard_post_init_user(void) { rgblight_layers = my_rgb_layers; }
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  rgblight_set_layer_state(0, layer_state_cmp(state, GAME));
+  rgblight_set_layer_state(1, layer_state_cmp(state, GAME_S));
+  return state;
+}
+
+// clang-format off
 
 /* Layout
     KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,                                             KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
@@ -55,17 +85,6 @@ combo_t key_combos[] = {
     KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,                                             KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
                                                                         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,           KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
 */
-
-#define BASE 0
-#define NAV 1
-#define SYM 2
-#define MOUSE 3
-#define GAME 4
-#define GAME_S 5
-#define MEDIA 6
-
-#define U_UND LCTL(KC_Z)
-#define U_RDO LCTL(KC_Y)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_5x6_54(
@@ -118,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [MEDIA] = LAYOUT_5x6_54(
     _______,         KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                      KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          QK_BOOT,
-    _______,         KC_VOLD,         KC_VOLU,         KC_MUTE,         TAB_AND_REFRESH, KC_F11,                                                     ALT_SHIFT,       KC_7,            KC_8,            KC_9,            _______,         _______,
+    _______,         KC_VOLD,         KC_VOLU,         KC_MUTE,         TAB_AND_REFRESH, KC_F11,                                                     ALT_SHIFT,       KC_7,            KC_8,            KC_9,            _______,         QK_UNDERGLOW_TOGGLE,
     _______,         KC_MPLY,         KC_MPRV,         KC_MNXT,         PASTE_ENTER,     KC_F12,                                                     ALT_SHIFT,       KC_4,            KC_5,            KC_6,            _______,         _______,
     _______,         KC_F13,          KC_F14,          KC_F15,          KC_F16,          _______,                                                    _______,         KC_1,            KC_2,            KC_3,            CW_TOGG,         _______,
                                                                         TG(GAME),        TG(GAME_S),      _______,                _______,           KC_0,            _______
